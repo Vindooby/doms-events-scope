ed4283ab5ca81cba3ecb71d1cf54d7f427273bf2
Vincent Wetzel <vincent.wetzel14@gmail.coms>
2017-04-02 18:30:20 -0400

    fixed errors for completion

--- main.js
+++ main.js
@@ -1,31 +1,31 @@
-// Question 1//
-//
-// function button1() {
-// document.getElementById('response').innerText = "I'm right!";
-// }
-//
-// function button2() {
-// document.getElementById('response').innerText = "No I'm right!"
-// }
+Question 1//
+
+function button1(click) {
+document.getElementById('response').innerText = "I'm right!";
+}
+
+function button2(click) {
+document.getElementById('response').innerText = "No I'm right!"
+}
 
 
-// Question 2//
+Question 2//
 
-//
-// document.getElementById("hoverme").addEventListener("mouseenter",function(){
-//   alert("Hey,I told you not to hover over me")
-// })
+
+document.getElementById("hoverme").addEventListener("mouseenter",function(){
+  alert("Hey,I told you not to hover over me")
+})
 
 
 // Question 3//
-    // var bruins=document.getElementById("h1")
-    //
-    // window.addEventListener("keypress",function(event){
-    //   console.log(event)
-    // bruins.in qnerHTML=event.key
-    // })
+    var bruins=document.getElementById("formh1")
 
-//Question 4//
+    window.addEventListener("keypress",function(event){
+      console.log(event)
+    bruins.innerHTML=event.key
+    })
+
+Question 4//
 var username = document.getElementById("username").value;
 var password = document.getElementById("password").value;
 var form = document.getElementById("form")
@@ -33,7 +33,8 @@ function checkForm(){
 
   if (password == 12345678) {
         if (username.length > 1) {
-            document.getElementById('formh1').innerHTML = changeHead;
+            document.getElementById('formh1').innerHTML = "Congrats on knowing
+your user name and password"//;
         } else {
             alert("Incorrect username");
         }
